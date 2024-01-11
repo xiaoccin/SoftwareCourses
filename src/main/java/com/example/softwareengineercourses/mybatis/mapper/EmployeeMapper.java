7634f2cf5669065b654b76a3a310258ef80e321c
xiaocc <1062353900@qq.com>
2024-01-11 09:35:55 +0800

    添加了对排班规则表的curd

--- src/main/java/com/example/softwareengineercourses/mybatis/mapper/EmployeeMapper.java
+++ src/main/java/com/example/softwareengineercourses/mybatis/mapper/EmployeeMapper.java
@@ -2,10 +2,9 @@ package com.example.softwareengineercourses.mybatis.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.softwareengineercourses.mybatis.pojo.Employee;
-import com.example.softwareengineercourses.mybatis.pojo.ScheduleRules;
 import org.apache.ibatis.annotations.*;
 
-import java.util.List;
+
 
 @Mapper
 public interface EmployeeMapper extends BaseMapper<Employee> {
